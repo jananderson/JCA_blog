24a287bde45349676e437a96f4fbf3549bb5e651
Jan Anderson <jchristiananderson@gmail.com>
2020-01-17 09:48:49 -0500

    Initial commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,56 @@
+namespace JCA_blog.Migrations
+{
+    using JCA_blog.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    internal sealed class Configuration : DbMigrationsConfiguration<JCA_blog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "JCA_blog.Models.ApplicationDbContext";
+        }
+        protected override void Seed(Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            var roleManager = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            if (!context.Roles.Any(r => r.Name == "Moderator"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Moderator" });
+            }
+            if (!context.Roles.Any(r => r.Name == "User"))
+            {
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+            if (!context.Roles.Any(r => r.Name == "Guest"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Guest" });
+            }
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!context.Users.Any(u => u.Email == "laymanscode@gmail.com"))
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "moderator@coderfoundry.com",
+                    Email = "moderator@coderfoundry.com",
+                    FirstName = "CF",
+                    LastName = "MODERATOR",
+                    DisplayName = "CFMOD"
+                }, "Abc&123!");
+            }
+            var userId = userManager.FindByEmail("jchristiananderson@gmail.com").Id;
+            userManager.AddToRole(userId, "Admin");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JCA_blog.Startup))]
+namespace JCA_blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
